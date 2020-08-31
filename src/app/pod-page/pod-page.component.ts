d89f24b9774ea4f4846a1a9785497ba44e257bb8
Patrick Sudol <patrick.sudol@outlook.com>
2020-08-30 21:34:51 -0400

    Progress on pod page component

--- src/app/pod-page/pod-page.component.ts
+++ src/app/pod-page/pod-page.component.ts
@@ -18,30 +18,56 @@ export class PodPageComponent implements OnInit {
   ngOnInit(): void {
 
     this.galleryOptions = [
-      {
-        width: '600px',
-        height: '400px',
-        thumbnailsColumns: 4,
-        imageAnimation: NgxGalleryAnimation.Slide
-      },
-      // max-width 800
-      {
-        breakpoint: 800,
-        width: '100%',
-        height: '600px',
-        imagePercent: 80,
-        thumbnailsPercent: 20,
-        thumbnailsMargin: 20,
-        thumbnailMargin: 20
-      },
-      // max-width 400
-      {
-        breakpoint: 400,
-        preview: false
-      }
+      { "image": false, "thumbnailsRemainingCount": true, "height": "100px" },
+      { "breakpoint": 500, "width": "100%", "thumbnailsColumns": 2 }
     ];
 
     this.galleryImages = [
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
+      {
+        small: 'assets/images/puertorico.png',
+        medium: 'assets/images/puertorico.png',
+        big: 'assets/images/puertorico.png'
+      },
       {
         small: 'assets/images/puertorico.png',
         medium: 'assets/images/puertorico.png',
