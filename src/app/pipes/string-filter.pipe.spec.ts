2af257d98d210823e8960eaa18452619ce92a789
Patrick Sudol <patrick.sudol@outlook.com>
2020-09-13 17:18:29 -0400

    StringFilter pipe unit tests

--- src/app/pipes/string-filter.pipe.spec.ts
+++ src/app/pipes/string-filter.pipe.spec.ts
@@ -1,8 +1,35 @@
 import { StringFilterPipe } from './string-filter.pipe';
 
 describe('StringFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StringFilterPipe();
-    expect(pipe).toBeTruthy();
+  const pipe = new StringFilterPipe();
+  const testArray: string[] = [
+    'Vertuo',
+    'Original',
+    'Gran Lungo',
+    'Barista Creations',
+    'Alto XL',
+    'Espresso',
+    'Double Espresso'
+  ];
+
+  it('should return an array with values with containing characters given', () => {
+    expect(pipe.transform(testArray, '')).toEqual(testArray);
+    expect(pipe.transform(testArray, 'Vertuo')).toEqual(['Vertuo']);
+    // TODO: Should update so pipe returns no results with a query with just spaces.
+    expect(pipe.transform(testArray, ' ')).toEqual(
+      ['Gran Lungo', 'Barista Creations', 'Alto XL', 'Double Espresso']
+    );
+    expect(pipe.transform(testArray, 'a')).toEqual(
+      ['Original', 'Gran Lungo', 'Barista Creations', 'Alto XL']
+    );
+    expect(pipe.transform(testArray, 'ar')).toEqual(['Barista Creations']);
+    expect(pipe.transform(testArray, 'Lungo')).toEqual(['Gran Lungo']);
+    expect(pipe.transform(testArray, 'inal')).toEqual(['Original']);
+    expect(pipe.transform(testArray, 'Keurig')).toEqual([]);
+    expect(pipe.transform(testArray, 'Vertua')).toEqual([]);
+  });
+
+  it('should return results that are case insensitive', () => {
+    expect(pipe.transform(testArray, 'vErTuo')).toEqual(['Vertuo']);
   });
 });
--- src/app/pipes/string-filter.pipe.ts
+++ src/app/pipes/string-filter.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+// TODO: Trim white space
 @Pipe({
   name: 'stringFilter'
 })
